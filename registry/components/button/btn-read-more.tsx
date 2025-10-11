5f28688d0b5adbcb8b0f8a47b5373096e08de429
nulla-dev <numjsdev@gmail.com>
2025-10-11 15:36:45 +0530

    feat(buttons): Refactor BtnReadMore component to use anchor tag and accept props for label, link, target, and className

--- registry/components/button/btn-read-more.tsx
+++ registry/components/button/btn-read-more.tsx
@@ -1,6 +1,6 @@
-const BtnReadMore = () => {
+const BtnReadMore = ({ label = "Read More", link = "#", target = "_self", className = ""}) => {
   return (
-    <button className="w-full">
+    <a href={link} target={target} className={`w-full ${className}`}>
       <div className="relative inline-flex items-center justify-start py-2 pl-4 pr-12 overflow-hidden font-semibold text-cyan-600 transition-all duration-150 ease-in-out rounded hover:pl-10 hover:pr-6 bg-gray-50 group w-full">
         <span className="absolute bottom-0 left-0 w-full h-1 transition-all duration-150 ease-in-out bg-indigo-600 group-hover:h-full bg-gradient-to-r from-cyan-500 to-blue-600"></span>
         <span className="absolute right-0 pr-4 duration-200 ease-out group-hover:translate-x-12">
@@ -14,10 +14,10 @@ const BtnReadMore = () => {
           </svg>
         </span>
         <span className="relative w-full text-left transition-colors duration-200 ease-in-out group-hover:text-white">
-          Read More
+          {label}
         </span>
       </div>
-    </button>
+    </a>
   );
 };
 
